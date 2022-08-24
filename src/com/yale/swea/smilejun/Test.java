1793b79dbd0b23f8034eed04af0e2f7d5c99b87f
DESKTOP-KVCQHCD\SSAFY <godhkekf24@inha.edu>
2022-08-24 20:58:54 +0900

    first setting

---
+++ src/com/yale/swea/smilejun/Test.java
@@ -0,0 +1,11 @@
+package com.yale.swea.smilejun;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이것도 되거든 바보야");
+		
+	}
+
+}
